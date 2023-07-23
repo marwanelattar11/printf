8b059d4aac17e5f2ddf159cca1deeb97052f9574
AHMED <ahmedalaa277143@gmail.com>
2023-07-23 13:21:17 +0000

    b

--- _strlen.c
+++ _strlen.c
@@ -1,1 +1,14 @@
-/*AHMED*/
+/**
+ * _strlen - length of a string
+ * @s: the string
+ * Return: the length
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (0);
+}
