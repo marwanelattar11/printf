95a6e0d4c02c77f198cb87585b70b4f317e5e17b
marwanelattar11 <marwanel3ttar@gmail.com>
2023-07-23 12:00:29 +0000

    a

---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdlib.h>
+#define NULL_STRING "(null)"
+
+
+
+int _printf(const char *format, ...);
+int print_char(va_list ap, params_t *params);
+int print_int(va_list ap, params_t *params);
+int print_string(va_list ap, params_t *params);
+int print_precent(va_list ap, params_t *params);
+#endif
